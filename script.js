522d9eaec9b439b6bb9e905a6cf94dd09179232d
SungJunEun <60536942+SungJunEun@users.noreply.github.com>
2021-09-27 13:36:56 +0000

    scroll button done

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+const intro = document.querySelector('.intro');
+const intro_y = intro.getBoundingClientRect().y+ window.pageYOffset;
+
+const intro_btn = document.querySelector('#intro.nav-menu-item');
+intro_btn.addEventListener('click', ()=> {
+  window.scroll(0,intro_y);
+});
+
+const contact_btn = document.querySelector('#contact.nav-menu-item');
+contact_btn.addEventListener('click', ()=> {
+  window.scroll(0,document.body.scrollHeight);
+});
