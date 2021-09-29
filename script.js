b2bb00b7c807269262ca0f1dfb417b01a7eadccd
SungJunEun <60536942+SungJunEun@users.noreply.github.com>
2021-09-29 13:32:24 +0000

    fix toggle menu button issue

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ const contact_btn = document.querySelector('#contact.nav-menu-item');
 contact_btn.addEventListener('click', ()=> {
   window.scroll(0,document.body.scrollHeight);
 });
+
+const menu_btn = document.querySelector('.menu-btn');
+const header = document.querySelector('.header');
+const menu_wide = document.createElement('menu_wide');
+menu_wide.setAttribute('class', 'menu-wide');
+const intro_btn2 = intro_btn.cloneNode(true);
+const contact_btn2 = contact_btn.cloneNode(true);
+intro_btn2.setAttribute('class', 'menu-wide-item');
+contact_btn2.setAttribute('class', 'menu-wide-item');
+menu_wide.appendChild(intro_btn2);
+menu_wide.appendChild(contact_btn2);
+document.body.insertBefore(menu_wide, header);
+console.log(menu_wide);
+menu_btn.addEventListener('click', ()=>{
+  eventhandler();
+
+});
+function eventhandler() {
+  menu_wide.classList.toggle('menu-wide-show');
+}
