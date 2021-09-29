a830023d6cb449ca8542c375139af63dd8658371
SungJunEun <60536942+SungJunEun@users.noreply.github.com>
2021-09-29 14:08:03 +0000

    fix scroll position bug

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 'use strict';
-const intro = document.querySelector('.intro');
-const intro_y = intro.getBoundingClientRect().y+ window.pageYOffset;
+function addscrollevent(intro,contact) {
+  intro.addEventListener('click', ()=> {
+    window.scrollTo(0,intro_container.getBoundingClientRect().y);
+    console.log(intro_container.getBoundingClientRect().y);
+    console.log(window.scrollY);
+  }); 
+  contact.addEventListener('click', ()=> {
+    window.scroll(0,document.body.scrollHeight);
+  });
+}
+const intro_container = document.querySelector('.intro');
+
 
 const intro_btn = document.querySelector('#intro.nav-menu-item');
-intro_btn.addEventListener('click', ()=> {
-  window.scroll(0,intro_y);
-});
-
 const contact_btn = document.querySelector('#contact.nav-menu-item');
-contact_btn.addEventListener('click', ()=> {
-  window.scroll(0,document.body.scrollHeight);
-});
+addscrollevent(intro_btn,contact_btn);
 
 const menu_btn = document.querySelector('.menu-btn');
 const header = document.querySelector('.header');
@@ -18,6 +22,7 @@ const menu_wide = document.createElement('menu_wide');
 menu_wide.setAttribute('class', 'menu-wide');
 const intro_btn2 = intro_btn.cloneNode(true);
 const contact_btn2 = contact_btn.cloneNode(true);
+addscrollevent(intro_btn2, contact_btn2);
 intro_btn2.setAttribute('class', 'menu-wide-item');
 contact_btn2.setAttribute('class', 'menu-wide-item');
 menu_wide.appendChild(intro_btn2);
@@ -32,9 +37,7 @@ function eventhandler() {
 }
 window.addEventListener('resize', ()=>{
   let media = window.matchMedia("screen and (min-width: 800px)");
-if (media.matches) {
-  (menu_wide.className == 'menu-wide show') && menu_wide.classList.remove('show');
-}
+  if (media.matches) {
+    (menu_wide.className == 'menu-wide show') && menu_wide.classList.remove('show');
+  }
 });
-
-console.log(media.matches);
