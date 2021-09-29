15c3e1e77246a6a0b2d7eee0444ee81e144e26c5
SungJunEun <60536942+SungJunEun@users.noreply.github.com>
2021-09-29 14:13:15 +0000

    refactoring scroll part of JS

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 'use strict';
-function addscrollevent(intro,contact) {
-  intro.addEventListener('click', ()=> {
-    window.scrollTo(0,intro_container.getBoundingClientRect().y);
-    console.log(intro_container.getBoundingClientRect().y);
-    console.log(window.scrollY);
-  }); 
-  contact.addEventListener('click', ()=> {
-    window.scroll(0,document.body.scrollHeight);
-  });
-}
+const header = document.querySelector('.header');
 const intro_container = document.querySelector('.intro');
-
 
 const intro_btn = document.querySelector('#intro.nav-menu-item');
 const contact_btn = document.querySelector('#contact.nav-menu-item');
 addscrollevent(intro_btn,contact_btn);
 
-const menu_btn = document.querySelector('.menu-btn');
-const header = document.querySelector('.header');
+function addscrollevent(intro,contact) {
+  intro.addEventListener('click', ()=> {
+    window.scrollTo(0,intro_container.getBoundingClientRect().y+window.pageYOffset);
+  }); 
+  contact.addEventListener('click', ()=> {
+    window.scroll(0,document.body.scrollHeight);
+  });
+}
+
 const menu_wide = document.createElement('menu_wide');
 menu_wide.setAttribute('class', 'menu-wide');
+
 const intro_btn2 = intro_btn.cloneNode(true);
-const contact_btn2 = contact_btn.cloneNode(true);
-addscrollevent(intro_btn2, contact_btn2);
 intro_btn2.setAttribute('class', 'menu-wide-item');
+const contact_btn2 = contact_btn.cloneNode(true);
 contact_btn2.setAttribute('class', 'menu-wide-item');
+addscrollevent(intro_btn2, contact_btn2);
+
 menu_wide.appendChild(intro_btn2);
 menu_wide.appendChild(contact_btn2);
 document.body.insertBefore(menu_wide, header);
-console.log(menu_wide);
+
+const menu_btn = document.querySelector('.menu-btn');
 menu_btn.addEventListener('click', ()=>{
-  eventhandler();
-});
-function eventhandler() {
   menu_wide.classList.toggle('show');
-}
+});
+
 window.addEventListener('resize', ()=>{
   let media = window.matchMedia("screen and (min-width: 800px)");
   if (media.matches) {
