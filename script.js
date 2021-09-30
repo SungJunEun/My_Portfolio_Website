09c53a4883ce7edfd591385ca0db6db361ca6869
SungJunEun <60536942+SungJunEun@users.noreply.github.com>
2021-09-30 13:28:36 +0000

    change JS into html parts

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 'use strict';
 const header = document.querySelector('.header');
 const intro_container = document.querySelector('.intro');
-
 const intro_btn = document.querySelector('#intro.nav-menu-item');
 const contact_btn = document.querySelector('#contact.nav-menu-item');
+const menu_wide = document.querySelector('.menu-wide');
+const menu_wide_intro_btn = document.querySelector('#intro.menu-wide-item');
+const menu_wide_contact_btn = document.querySelector('#contact.menu-wide-item');
+const menu_btn = document.querySelector('.menu-btn');
+
 addscrollevent(intro_btn,contact_btn);
+addscrollevent(menu_wide_intro_btn,menu_wide_contact_btn);
+
+menu_btn.addEventListener('click', ()=>{
+  menu_wide.classList.toggle('show');
+});
+window.addEventListener('resize', ()=>{
+  let media = window.matchMedia("screen and (min-width: 800px)");
+  if (media.matches) {
+    (menu_wide.className == 'menu-wide show') && menu_wide.classList.remove('show');
+  }
+});
 
 function addscrollevent(intro,contact) {
   intro.addEventListener('click', ()=> {
@@ -15,27 +30,5 @@ function addscrollevent(intro,contact) {
   });
 }
 
-const menu_wide = document.createElement('menu_wide');
-menu_wide.setAttribute('class', 'menu-wide');
 
-const intro_btn2 = intro_btn.cloneNode(true);
-intro_btn2.setAttribute('class', 'menu-wide-item');
-const contact_btn2 = contact_btn.cloneNode(true);
-contact_btn2.setAttribute('class', 'menu-wide-item');
-addscrollevent(intro_btn2, contact_btn2);
 
-menu_wide.appendChild(intro_btn2);
-menu_wide.appendChild(contact_btn2);
-document.body.insertBefore(menu_wide, header);
-
-const menu_btn = document.querySelector('.menu-btn');
-menu_btn.addEventListener('click', ()=>{
-  menu_wide.classList.toggle('show');
-});
-
-window.addEventListener('resize', ()=>{
-  let media = window.matchMedia("screen and (min-width: 800px)");
-  if (media.matches) {
-    (menu_wide.className == 'menu-wide show') && menu_wide.classList.remove('show');
-  }
-});
